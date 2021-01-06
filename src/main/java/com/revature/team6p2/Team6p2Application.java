45e6a0e2942903265688339e8146c5fabd882e0f
jeevanselva <75275273+jeevanselva@users.noreply.github.com>
2021-01-06 00:19:02 -0500

    Initial spring boot application

---
+++ src/main/java/com/revature/team6p2/Team6p2Application.java
@@ -0,0 +1,13 @@
+package com.revature.team6p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team6p2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team6p2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/team6p2/Team6p2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.team6p2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team6p2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
