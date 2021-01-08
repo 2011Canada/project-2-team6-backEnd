59f496a803a1b042392adbc389032381c346df40
Hassen Rammal <rammal1@uwindsor.ca>
2021-01-08 14:01:10 -0500

    fixed refactoring

--- RevBookCompany/src/main/java/com/revature/RevBookCompanyApplication.java
+++ RevBookCompany/src/main/java/com/revature/RevBookCompanyApplication.java
@@ -7,10 +7,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class DmPaperCompanyApplication {
+public class RevBookCompanyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DmPaperCompanyApplication.class, args);
+		SpringApplication.run(RevBookCompanyApplication.class, args);
 	}
 
 }
