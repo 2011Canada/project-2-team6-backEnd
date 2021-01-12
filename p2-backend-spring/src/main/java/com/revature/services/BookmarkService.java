4cbc739e51467c189c0871862b375b54dc7d9906
Stephen491 <stephen.wong@revature.net>
2021-01-12 12:03:03 -0500

    bookmarks backend

---
+++ p2-backend-spring/src/main/java/com/revature/services/BookmarkService.java
@@ -0,0 +1,33 @@
+package com.revature.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.models.Bookmark;
+import com.revature.models.User;
+import com.revature.repositories.BookmarkDao;
+
+
+@Service
+public class BookmarkService {
+	
+	BookmarkDao bd; 
+	
+	@Autowired
+	public BookmarkService(BookmarkDao bd) {
+		this.bd = bd;
+	}
+	
+	public Bookmark saveBookmark(Bookmark b) {
+		return bd.saveAndFlush(b);
+	}
+	
+	public List<Bookmark> findBookmarksByUserId(int user_id) {
+		return bd.findByUserId(user_id);
+	}
+	
+	
+	
+}
