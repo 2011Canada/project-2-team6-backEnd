14232d172c88fb1592c4008586c913048298af62
jeevanselva <75275273+jeevanselva@users.noreply.github.com>
2021-01-17 04:43:10 -0500

    Modify Bookmark Controller

--- p2-backend-spring/src/main/java/com/revature/controllers/BookmarkController.java
+++ p2-backend-spring/src/main/java/com/revature/controllers/BookmarkController.java
@@ -1,22 +1,23 @@
 package com.revature.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.revature.models.Bookmark;
-import com.revature.services.BookmarkService;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.models.Bookmark;
+import com.revature.services.BookmarkService;
+
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 @RestController
 @RequestMapping("/bookmarks")
 public class BookmarkController {
